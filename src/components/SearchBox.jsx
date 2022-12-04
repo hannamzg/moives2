1db3de7c5e21edb41d34c892748faa9729e0e22a
hanna mzeget <elimzghanna123@gmail.com>
2022-12-04 15:48:32 +0200

    w

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -5,14 +5,16 @@ function SearchBox(props) {
     return (
         <div className="col col-sm-4 mt-3" style={{display:"flex",justifyContent:"end",flexDirection:"column",overflow:"hidden",position:"relative"}} > 
             <div style={{position:"relative"}}>
-                <input type="text" className='form-control' value={props.searchValue} onChange={(event) =>/*  props.setSearchValue(event.target.value)  */ alert("fuckkkkkkkkkkkkkkk")} placeholder='Type to search...' style={{width:"100%"}}/>
+                <input type="text" className='form-control' value={props.searchValue} onChange={(event) => props.setSearchValue(event.target.value)  } placeholder='Type to search...' style={{width:"100%"}}/>
                 <i className="bi bi-x-circle" style={{position:"absolute",color:"black",zIndex:"12",top:"7px",right:'10px',cursor:"pointer"}} onClick={()=>props.setSearchValue("")}></i>
             </div>
 
             <ul style={{display:"flex",justifyContent:"start",flexDirection:"column",width:"100%"}}>
                 {props.movies.map((movie,index)=>( props.searchValue && <NavLink to="/MoviePage" key={index}> <li className="dropdown-item" key={index} style={{color:"white"}} onClick={()=> props.AddsetMoviePage(movie)}>{movie.Title}</li></NavLink>))}
             </ul>
-
+        <div onClick={()=>alert("fuckkkkkkkkkkkkkkk")}>
+            <h1>click me</h1>
+        </div>
         </div>
     )    
 }
